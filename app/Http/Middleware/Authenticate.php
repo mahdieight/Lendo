0cc052dfb6486cc69aed1fffdcf194273f720bd7
Mahdi Amereh <mahdieightt@gmail.com>
2024-07-20 17:08:47 +0330

    fix: fix Authenticate error  for api request

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,7 +3,9 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Auth\Middleware\Authenticate as MiddlewareAuthenticate;
 use Illuminate\Http\Request;
+use Illuminate\Validation\UnauthorizedException;
 
 class Authenticate extends Middleware
 {
@@ -12,6 +14,8 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        return $request->expectsJson() ? null : throw new UnauthorizedException();
+
+        
     }
 }
